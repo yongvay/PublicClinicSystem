05eff464c299debac20e3c5a8365a0fd4f196e30
LAPTOP-F8B6GE18\PC <chan.xingszen@gmail.com>
2026-03-06 14:29:32 +0800

    Doctor Module

---
+++ src/main/java/Control/DoctorRepository.java
@@ -0,0 +1,32 @@
+package Control;
+
+import ADT.ListInterface;
+import Entity.Doctor;
+
+/**
+ * @author Xing Szen
+ */
+public interface DoctorRepository {
+
+    // Create 
+    void create(Doctor doctor);
+
+    // Read (searchDoctor, getAllDoctors, getAvailableDoctor)
+    ListInterface<Doctor> findAll();
+    Doctor findById(String id);
+    Doctor findFirstAvailableDoctor(); // fetch one free doctor for the queue
+    
+    //finders
+    ListInterface<Doctor> findBySpecialization(String specialization);
+    ListInterface<Doctor> findAllAvailableDoctors();
+
+    // Update (updateDoctorDetails)
+    boolean update(Doctor doctor);
+
+    // Delete (deleteDoctor)
+    boolean delete(Doctor doctor);
+    
+    //Sorting methods
+    ListInterface<Doctor> findAllSortedByName();
+    ListInterface<Doctor> findAllSortedBySpecialization();
+}
